d13957e25d1c37688f8396419528d609cf0b8b11
Vitor <vitorlopesdesouza@gmail.com>
2018-08-20 14:15:34 -0300

    challenge-26

--- challenge-26/challenge-26.js
+++ challenge-26/challenge-26.js
@@ -1,4 +1,5 @@
-/*
+(function(win, doc) {
+  /*
 O desafio dessa semana é criar uma mini library (biblioteca) para
 reutilizarmos nossos códigos quando fizermos manipulação de DOM!
 
@@ -19,13 +20,36 @@ selecionados.
 Dica: olhe os erros que acontecem no console, e vá resolvendo um a um.
 Só passe para o próximo problema quando tiver resolvido o anterior :)
 */
-// ?
+  function DOM(elements) {
+    this.element = doc.querySelectorAll(elements);
+  }
 
-var $a = new DOM('[data-js="link"]');
-$a.on('click', function(e) {
-  e.preventDefault();
-  console.log('clicou');
-});
+  DOM.prototype.on = function on(event, callback) {
+    this.element.forEach(function(element) {
+      element.addEventListener(event, callback, false);
+    });
+  };
 
-console.log('Elementos selecionados:', $a.get());
-console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+  DOM.prototype.off = function off(event, callback) {
+    this.element.forEach(function(element) {
+      element.removeEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function get() {
+    return this.element;
+  };
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('clicou');
+  }
+
+  var $a = new DOM('[data-js="link"]');
+  $a.on('click', handleClick);
+
+  $a.off('click', handleClick);
+
+  console.log('Elementos selecionados:', $a.get());
+  console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+})(window, document);

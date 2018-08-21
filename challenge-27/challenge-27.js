752a6ea98e873a8c2aacde382c070adfc50bc54d
Vitor <vitorlopesdesouza@gmail.com>
2018-08-21 14:15:01 -0300

    refactor : challenge-27

--- challenge-27/challenge-27.js
+++ challenge-27/challenge-27.js
@@ -1,4 +1,6 @@
-/*
+(function(win, doc) {
+  'use strict';
+  /*
 Aproveitando a lib DOM que fizemos na semana anterior, crie agora para ela
 métodos semelhantes aos que existem no array, mas que sirvam para os
 elementos do DOM selecionados.
@@ -19,3 +21,67 @@ Crie os seguintes métodos para verificação de tipo:
 - isArray, isObject, isFunction, isNumber, isString, isBoolean, isNull.
 O método isNull deve retornar `true` se o valor for null ou undefined.
 */
+
+  function DOM(elements) {
+    this.element = doc.querySelectorAll(elements);
+  }
+
+  DOM.prototype.on = function on(event, callback) {
+    this.element.forEach(function(element) {
+      element.addEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.off = function off(event, callback) {
+    this.element.forEach(function(element) {
+      element.removeEventListener(event, callback, false);
+    });
+  };
+
+  DOM.prototype.get = function get() {
+    return this.element;
+  };
+
+  DOM.prototype.applyMethod = function applyMethod(method, callback) {
+    switch (method) {
+      case 'forEach':
+        return Array.prototype.forEach.call(this.element, callback);
+      case 'map':
+        return Array.prototype.map.call(this.element, callback);
+      case 'filter':
+        return Array.prototype.filter.call(this.element, callback);
+      case 'reduce':
+        return Array.prototype.reduce.call(this.element, callback);
+      case 'reduceRight':
+        return Array.prototype.reduceRight.call(this.element, callback);
+      case 'every':
+        return Array.prototype.every.call(this.element, callback);
+      case 'some':
+        return Array.prototype.some.call(this.element, callback);
+    }
+  };
+
+  DOM.prototype.verifyType = function verifyType(param, type) {
+    return Object.prototype.toString.call(param) === '[object ' + type + ']';
+  };
+
+  function handleClick(e) {
+    e.preventDefault();
+    console.log('clicou');
+  }
+
+  var $a = new DOM('[data-js="link"]');
+  $a.on('click', handleClick);
+
+  $a.off('click', handleClick);
+
+  $a.applyMethod('map', function(item) {
+    console.log(item);
+  });
+
+  var dom = new DOM();
+  console.log(dom.verifyType([1, 2, 3], 'Array'));
+
+  console.log('Elementos selecionados:', $a.get());
+  console.log('$a é filho de body?', $a.get()[0].parentNode === document.body);
+})(window, document);
